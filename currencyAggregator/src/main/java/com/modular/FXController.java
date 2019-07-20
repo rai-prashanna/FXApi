d50cb49672b6ea7755734ba002c726b645ffbf24
prashanna <prai93@outlook.com>
2019-07-20 19:48:28 +0200

    refactored controller

--- currencyAggregator/src/main/java/com/modular/FXController.java
+++ currencyAggregator/src/main/java/com/modular/FXController.java
@@ -1,9 +1,8 @@
 package com.modular;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,39 +11,41 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import sun.util.logging.resources.logging;
-
 @RestController
 public class FXController {
-	  private static final Logger log = LoggerFactory.getLogger(FXController.class);
+	private static final Logger log = LoggerFactory
+			.getLogger(FXController.class);
 
-	@GetMapping("/2010-01-01/2011-01-01/agg")
-	RestResponse findAll(@RequestParam String symbols,
-			@RequestParam String base, @RequestParam String func) {
-		RestResponse resposResponse = new RestResponse();
-		RestResponse response = null;
+	@GetMapping("/2018-01-01/2019-01-01/agg")
+	Forex findAll(@RequestParam String symbols, @RequestParam String base,
+			@RequestParam String func) {
+		Forex forex = new Forex();
+		Forex response = new Forex();
 		if (func.equals("median")) {
 			RestTemplate restTemplate = new RestTemplate();
+			//Since task was to median exchange rate for USD to SEK during last year
+			//i.e 2018-01-01 to 2019-01-01 so hard coded logic 
 			String strtDate = "2018-01-01";
 			String endDate = "2019-01-01";
 			MedianCalculator median = new MedianCalculator();
 			double result = 0;
-			
-			//double[] streaming = new double[365];
-			int currentIndex=0;
-			for (LocalDate date = LocalDate.parse(strtDate); date.isBefore(LocalDate.parse(endDate)); date = date.plusDays(1))
-			{
-			response = restTemplate
-					.getForObject(
-							"http://fx.modfin.se/"+date.toString()+"/?symbols=sek&base=usd",
-							RestResponse.class);
-			//streaming[currentIndex]=response.getRates().getSEK();
-			result=median.getMedian(response.getRates().getSEK());	
-			
-			//streaming.add(response.getRates().getSEK());
-			}	
-			System.out.println("the median is "+result);
 
+			for (LocalDate date = LocalDate.parse(strtDate); date
+					.isBefore(LocalDate.parse(endDate)); date = date
+					.plusDays(1)) {
+				forex = restTemplate.getForObject(
+						"http://fx.modfin.se/" + date.toString()
+								+ "/?symbols=sek&base=usd", Forex.class);
+
+				result = median.getMedian(forex.getRates().get("SEK"));
+
+			}
+			response.setBase("USD");
+			response.setFun("median");
+
+			Map<String, Double> rates=new HashMap<String, Double>();
+			rates.put("SEK", result);
+			response.setRates(rates);;
 		}
 
 		return response;
