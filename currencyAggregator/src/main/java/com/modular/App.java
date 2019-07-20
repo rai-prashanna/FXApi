dc6e62b71947c8019cf086e3ed804293ee6dc54e
prashanna <prai93@outlook.com>
2019-07-20 19:48:52 +0200

    refactoring

--- currencyAggregator/src/main/java/com/modular/App.java
+++ currencyAggregator/src/main/java/com/modular/App.java
@@ -3,14 +3,10 @@ package com.modular;
 
 
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class App {
